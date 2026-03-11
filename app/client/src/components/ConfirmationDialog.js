79c79b6f1a1221a8b152638805c7bd2d6fc37e74
Nicholas Guiley <nguiley36@gmail.com>
2026-03-10 21:43:01 -0700

    Add prop types to confirmation dialog

--- app/client/src/components/ConfirmationDialog.js
+++ app/client/src/components/ConfirmationDialog.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { Button, CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 function ConfirmationDialog({ open, title, body, onConfirm, onCancel, loading = false }) {
@@ -18,5 +19,14 @@ function ConfirmationDialog({ open, title, body, onConfirm, onCancel, loading = 
         </Dialog>
     );
 }
+
+ConfirmationDialog.propTypes = {
+    open: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    loading: PropTypes.bool,
+};
 
 export default ConfirmationDialog;
