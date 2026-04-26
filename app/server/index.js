f6ba0ff6bf517054a2e18bb3ffbe2b84d3e8c031
Nicholas Guiley <nguiley36@gmail.com>
2026-04-26 15:46:37 -0700

    Update ci and server test

--- app/server/index.js
+++ app/server/index.js
@@ -42,7 +42,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
+app.use((req, _res, next) => {
   logger.info("Request", { method: req.method, path: req.path });
   next();
 });
